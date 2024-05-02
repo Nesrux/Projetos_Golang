f802e87bd1fc11b4667dddccd06b7ddde54c7972
Nesrux <joaomarcosdevs@gmail.com>
2024-05-02 19:41:46 -0300

    feat: adiciona salvamento em memoria

--- Io/main.go
+++ Io/main.go
@@ -5,8 +5,14 @@ import (
 	"net/http"
 )
 
+type ArmazenamentoJogadorEmMemoria struct {}
+
+func (a *ArmazenamentoJogadorEmMemoria) ObterPontuacaoJogador(nome string) int {
+	return 123
+}
+
 func main() {
-	servidor := &ServidorJogador{}
+	servidor := &ServidorJogador{&ArmazenamentoJogadorEmMemoria{}}
 
 	if err := http.ListenAndServe(":8080", servidor); err != nil {
 		log.Fatalf("não foi possivel escutar a porta 8080 %v", err)
